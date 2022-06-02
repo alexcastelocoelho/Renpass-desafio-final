d7f9da06a38d20847db52d84261ea75004bd7571
Alex Castelo <alexcastelo437@gmail.com>
2022-06-02 18:26:32 -0300

    feat: create CarService

---
+++ src/app/service/CarService.js
@@ -0,0 +1,8 @@
+const CarRepository = require('../repository/CarRepository')
+
+class CarService {
+    async create(payload) {
+        const result = await CarRepository.create(payload)
+        return result
+    }
+}
