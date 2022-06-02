7fac4db2fe7b5ea3c4c81faac2ad0853bb467a28
Alex Castelo <alexcastelo437@gmail.com>
2022-06-02 15:07:18 -0300

    feat: configuring dotenv and creating mongodb connection

---
+++ src/infra/database/mongo/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const config = require('../../../config/config')
+
+class Database {
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        return mongoose.connect(
+            `mongodb://${config.database.host}/${config.database.collection}`
+        );
+    }
+
+}
+
+module.exports = new Database();
