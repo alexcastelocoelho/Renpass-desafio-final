18a96f13aa01739f3b7dcd07c90e21b5df0294cf
Alex Castelo <alexcastelo437@gmail.com>
2022-06-02 18:18:31 -0300

    feat: create CarSchema

---
+++ src/app/schema/CarSchema.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose')
+
+const CarSchema = new mongoose.Schema({
+    model: {
+        type: String,
+        required:true
+    },
+    
+    type: {
+        type:String,
+        required: true
+    },
+
+    brand: {
+        type: String,
+        required: true
+
+    },
+    
+    color: {
+        type: String,
+        required: true
+    },
+    
+    year: {
+        type: String,
+        required: true
+    },
+
+    accessories: [          
+         {
+            description: {
+                type: String
+            }
+        }
+    ],
+    passengersQtd: {
+        type: Number,
+        required: true
+    }
+
+})
+
+const car = mongoose.model('Car', CarSchema)
+module.exports = car;
