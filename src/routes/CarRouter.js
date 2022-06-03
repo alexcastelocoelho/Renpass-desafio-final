472d0fe72f7eeacd40b4a80f1956256e9d76e490
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 15:34:50 -0300

    feat: adding get methods to car routes

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -2,6 +2,8 @@ const CarController = require('../app/controller/CarController')
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
     routes.post('/', CarController.creating);
+    routes.get('/', CarController.listing);
+    routes.get('/:id', CarController.listone);
 
 
     server.use(prefix, routes)
