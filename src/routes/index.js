eb4ba6096f22fe3802462bfd65045ce75fb0f106
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 18:08:20 -0300

    feat: added people routes in the main file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const { Router} = require('express')
 const Car = require('./CarRouter')
+const Person = require('./PersonRouter')
 module.exports = (server) => {
     server.use((req, res, next) => {
         Car(server, new Router())
+        Person(server, new Router())
+
         
         next();
     })
