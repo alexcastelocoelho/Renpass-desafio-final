3d38c649a4782235950f46a25b223d60b7640dbf
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 18:03:09 -0300

    feat: created PersonRepository file and PersonService file

---
+++ src/app/repository/PersonRepository.js
@@ -0,0 +1,26 @@
+const PersonSchema = require('../schema/PersonSchema')
+
+class PersonRepository {
+    async createPerson(payload){
+        return await PersonSchema.create(payload)
+    }
+    
+    async listPersons(payload) {
+        return await PersonSchema.find(payload)
+    }
+
+    async getPerson(payload) {
+        return await PersonSchema.findById(payload)
+    }
+
+    async updatePerson(id, payload) {
+        return await PersonSchema.findByIdAndUpdate(id, payload)
+    }
+
+    async deletePerson(payload) {
+        return await PersonSchema.findByIdAndDelete(payload)
+    }
+
+}
+
+module.exports = new PersonRepository()
