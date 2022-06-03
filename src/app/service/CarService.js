8545f3e0b1b467e1a134df9645487b253172239c
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 14:11:59 -0300

    feat: adding methods in CarService and importing CarRepository

--- src/app/service/CarService.js
+++ src/app/service/CarService.js
@@ -2,7 +2,28 @@ const CarRepository = require('../repository/CarRepository')
 
 class CarService {
     async create(payload) {
-        const result = await CarRepository.create(payload)
+        const result = await CarRepository.createCar(payload)
         return result
     }
+
+    async listAll(payload) {
+        const result = await CarRepository.listCars(payload)
+        return result
+    }
+
+    async getById(payload) {
+        const result = await CarRepository.getCar(payload)
+        return result 
+    }
+
+    async update(paylaod, id ) {
+        const result = await CarRepository.updateCar(paylaod, id)
+        return result
+    }
+
+    async delete(payload) {
+        const result = await CarRepository.deleteCar(payload) 
+    }
 }
+
+module.exports = new CarService(); 
