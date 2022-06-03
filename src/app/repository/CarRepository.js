2099d2d86a3fbb53d8a8e7de568da03a989cc544
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 13:24:01 -0300

    feat: added mongoose methods in carservice

--- src/app/repository/CarRepository.js
+++ src/app/repository/CarRepository.js
@@ -1,9 +1,26 @@
 const CarSchema = require('../schema/CarSchema')
 
 class CarRepository {
-    async create(payload){
+    async createCar(payload){
         return await CarSchema.create(payload)
     }
+    
+    async listCars(payload) {
+        return await CarSchema.find(payload)
+    }
+
+    async getCar(payload) {
+        return await CarSchema.findById(payload)
+    }
+
+    async updateCar(id, payload) {
+        return await CarSchema.findByIdAndUpdate(id, payload)
+    }
+
+    async deleteCar(payload) {
+        return await CarSchema.findByIdAndDelete(payload)
+    }
+
 }
 
 module.exports = new CarRepository()
