c12404f299cb8887f528b19a36f8a1e77314aa49
Alex Castelo <alexcastelo437@gmail.com>
2022-06-03 14:17:39 -0300

    feat: adicionando métodos em CarController e importando CarService

--- src/app/controller/CarController.js
+++ src/app/controller/CarController.js
@@ -1,7 +1,7 @@
-const CarService = require('../repository/CarRepository')
+const CarService = require('../service/CarService')
 
 class CarController {
-    async create(req, res) {
+    async creating(req, res) {
         try {
             const result = await CarService.create(req.body) 
             return res.status(201).json(result)
@@ -10,4 +10,41 @@ class CarController {
         }
     }
 
+    async listing(req, res) {
+        try {
+            const result = await CarService.listAll(req.query)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async listone(req, res) {
+        try {
+            const result = await CarService.getById(req.params.iod)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+
+    async updating(req, res) {
+        try {
+            const result = await CarService.update(req.params.id, req.body)
+        } catch (error) {
+            return res.status(400).json(error)
+        }
+    }
+    
+    async deleting(req, res) {
+        try {
+            const result = await CarService.delete(req.params.id)
+            return res.status(204).json(result)
+        } catch (error) {
+            return res.status(400).json(error) 
+        }
+    }
+
 }
+
+module.exports = new CarController();
