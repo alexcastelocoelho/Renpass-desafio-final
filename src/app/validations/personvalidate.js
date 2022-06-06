a12a6a72d868a1f6afe5e85660103d24b89c23c2
Alex Castelo <alexcastelo437@gmail.com>
2022-06-06 16:29:46 -0300

    fix: fixing people validation

--- src/app/validations/personvalidate.js
+++ src/app/validations/personvalidate.js
@@ -1,17 +1,22 @@
 const Joi = require('joi');
 
-const validPerson = Joi.object({
-	name:Joi.string().min(3).required(),
-	cpf: Joi.string().required(),
-	birthday: Joi.string().required(),
-	email: Joi.string().email().required(),
-	password: Joi.string().min(6).required(),
-	candrive: Joi.string().valid('yes', 'no').required()
+module.exports = async (req, res, next) => {
+	try{
+		
+		const validPerson = Joi.object({
+			name:Joi.string().min(3).required(),
+			cpf: Joi.string().required(),
+			birthday: Joi.string().required(),
+			email: Joi.string().email().required(),
+			password: Joi.string().min(6).required(),
+			candrive: Joi.string().valid('yes', 'no').required()
+			
+		});
 
-
-});
-
-module.exports = {
-	validPerson
-
+		const {error} = await validPerson.validate(req.body, {abortEarly: true});
+		if (error) throw error;
+		return next();
+	} catch (error){		
+		return res.status(400).json({Error: error.message});
+	}
 };
