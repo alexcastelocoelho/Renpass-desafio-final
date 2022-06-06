e54d6ff59a8d5b55ed5fe1fb1a6d42a967f4176c
Alex Castelo <alexcastelo437@gmail.com>
2022-06-06 18:43:33 -0300

    fix: correcting validation of people in the post route

--- src/routes/PersonRouter.js
+++ src/routes/PersonRouter.js
@@ -1,7 +1,7 @@
 const PersonController = require('../app/controller/PersonController');
-const validate = require('../app/validations/personvalidate');
+const validateperson = require('../app/validations/personvalidate');
 module.exports = (server, routes, prefix = '/api/v1/person') => {
-	routes.post('/',validate ,PersonController.creating);
+	routes.post('/', validateperson ,PersonController.creating);
 	routes.get('/', PersonController.listing);
 	routes.get('/:id', PersonController.listone);
 	routes.put('/:id', PersonController.updating);
