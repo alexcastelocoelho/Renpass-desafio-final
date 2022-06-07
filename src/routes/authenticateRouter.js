a86e68ff43816ba010816599f0597470d449a0bd
Alex Castelo <alexcastelo437@gmail.com>
2022-06-07 18:12:03 -0300

    feat: created file for authentication route

---
+++ src/routes/authenticateRouter.js
@@ -0,0 +1,8 @@
+const authenticateController = require('../app/controller/AuthenticateController');
+
+module.exports = (server, routes, prefix = '/api/v1/authenticate') => {
+	routes.post('/', authenticateController.create);
+	
+	server.use(prefix, routes);
+
+};
