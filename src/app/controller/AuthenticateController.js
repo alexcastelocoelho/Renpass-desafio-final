95db7f7f76667bc37967c47071785ef2c273a248
Alex Castelo <alexcastelo437@gmail.com>
2022-06-07 18:10:12 -0300

    feat: create classe Authenticate controller

---
+++ src/app/controller/AuthenticateController.js
@@ -0,0 +1,21 @@
+const AuthenticaService = require('../service/AuthenticateService');
+
+class AuthenticateController {
+    
+	async create(req, res) {
+		try{
+			
+			const email = req.body.email;
+			const password = req.body.password;
+			
+			const result = await AuthenticaService.Authenticate(email, password);
+			return res.status(201).json(result);
+		} catch (error) {
+			return res.status(400).json(error);
+		}
+	}
+        
+  
+}
+
+module.exports = new AuthenticateController();
