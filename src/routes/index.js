952a8b315ad3036b63319aef5a07b0b9752b734f
Alex Castelo <alexcastelo437@gmail.com>
2022-06-07 18:13:42 -0300

    feat: add authentication route file to the main route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router} = require('express');
 const Car = require('./CarRouter');
 const Person = require('./PersonRouter');
+const authenticate = require('./authenticateRouter');
 module.exports = (server) => {
 	server.use((req, res, next) => {
 		Car(server, new Router());
 		Person(server, new Router());
+		authenticate(server, new Router());
 
         
 		next();
