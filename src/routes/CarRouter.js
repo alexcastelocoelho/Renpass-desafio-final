a8f088bc3a7ba953279e508b219a27594442ab56
Alex Castelo <alexcastelo437@gmail.com>
2022-06-06 21:53:50 -0300

    feat: validation of the car created and added to the route

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -1,7 +1,7 @@
 const CarController = require('../app/controller/CarController');
-
+const validateCar = require('../app/validations/carvalidate');
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-	routes.post('/', CarController.creating);
+	routes.post('/', validateCar ,CarController.creating);
 	routes.get('/', CarController.listing);
 	routes.get('/:id', CarController.listone);
 	routes.put('/:id', CarController.updating);
