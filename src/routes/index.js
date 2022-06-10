b3814fa1714027ff6d9ee5263b85042766be979c
Alex Castelo <alexcastelo437@gmail.com>
2022-06-10 15:10:30 -0300

    feat: added rental routes to the main routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const { Router} = require('express');
 const Car = require('./CarRouter');
 const Person = require('./PersonRouter');
 const authenticate = require('./authenticateRouter');
+const Rental = require('./RentalRouter');
 module.exports = (server) => {
 	server.use((req, res, next) => {
 		Car(server, new Router());
 		Person(server, new Router());
 		authenticate(server, new Router());
-
+		Rental(server, new Router());
         
 		next();
 	});
