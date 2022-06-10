22ec951daea9884735b1f603e8b767542078cbbb
Alex Castelo <alexcastelo437@gmail.com>
2022-06-10 15:08:15 -0300

    feat: routes created for rental

---
+++ src/routes/RentalRouter.js
@@ -0,0 +1,13 @@
+const RentalController = require('../app/controller/RentalController');
+
+module.exports = (server, routes, prefix = '/api/v1/rental') => {
+	routes.post('/',RentalController.creating);
+	routes.get('/', RentalController.listing);
+	routes.get('/:id', RentalController.listone);
+	routes.put('/:id', RentalController.updating);
+	routes.delete('/:id', RentalController.deleting);
+
+
+	server.use(prefix, routes);
+
+};
