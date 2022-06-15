4491f70f59aef01e4373ffed808e8731d8b697c7
Alex Castelo <alexcastelo437@gmail.com>
2022-06-14 21:31:30 -0300

    feat: route created to upgrade car accessory

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -5,6 +5,7 @@ module.exports = (server, routes, prefix = '/api/v1/car') => {
 	routes.get('/', CarController.listing);
 	routes.get('/:id', CarController.listone);
 	routes.put('/:id', CarController.updating);
+	routes.patch('/:id/accessories/:idAccessorie', CarController.updatingAcessoryCar);
 	routes.delete('/:id', CarController.deleting);
 
 
