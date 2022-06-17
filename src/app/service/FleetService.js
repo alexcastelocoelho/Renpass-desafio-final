37af610a8774c39700fcf55db02cae4d2cf109e6
Alex Castelo <alexcastelo437@gmail.com>
2022-06-17 15:53:26 -0300

    feat: created fleet service

---
+++ src/app/service/FleetService.js
@@ -0,0 +1,29 @@
+const FleetRepository = require('../repository/FleetRepository');
+
+class FleetService {
+	async create(payload) {
+		const result = await FleetRepository.createFleet(payload);
+		return result;
+	}
+
+	async listAll(payload) {
+		const result = await FleetRepository.listFleet(payload);
+		return result;
+	}
+
+	async getById(payload) {
+		const result = await FleetRepository.getFleet(payload);
+		return result; 
+	}
+
+	async update(paylaod, id ) {
+		const result = await FleetRepository.updateFleet(paylaod, id);
+		return result;
+	}
+
+	async delete(payload) {
+		const result = await FleetRepository.deleteFleet(payload); 
+	}
+}
+
+module.exports = new FleetService(); 
