4de51a0178d25f0bece4a230c41877f857059376
Alex Castelo <alexcastelo437@gmail.com>
2022-06-17 10:21:51 -0300

    feat: reserve routes added in main routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ const Car = require('./CarRouter');
 const Person = require('./PersonRouter');
 const authenticate = require('./authenticateRouter');
 const Rental = require('./RentalRouter');
+const Reserve = require('./ReserveRouter');
 module.exports = (server) => {
 	server.use((req, res, next) => {
 		Car(server, new Router());
 		Person(server, new Router());
 		authenticate(server, new Router());
 		Rental(server, new Router());
+		Reserve(server,new Router());
         
 		next();
 	});
