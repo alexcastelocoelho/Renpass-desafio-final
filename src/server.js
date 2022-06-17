9c04d203690f64b8263adafe024d37c541a59c3c
Alex Castelo <alexcastelo437@gmail.com>
2022-06-17 17:59:32 -0300

    refactor: changing port variable to env

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const port = 3000;
+const ENV = 3000;
 
-app.listen(port, () => {
-	console.log(`server running on port ${port}`);
+app.listen(ENV, () => {
+	console.log(`server running on port ${ENV}`);
 });
