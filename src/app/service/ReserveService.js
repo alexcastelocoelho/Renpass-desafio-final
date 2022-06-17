cf4e18abb1a978ff95ec583c1687f5fb6c209a56
Alex Castelo <alexcastelo437@gmail.com>
2022-06-17 10:18:40 -0300

    feat: created reserve service class

---
+++ src/app/service/ReserveService.js
@@ -0,0 +1,29 @@
+const ReserveRepository = require('../repository/ReserveRepository');
+
+class RentalService {
+	async create(payload) {
+		const result = await ReserveRepository.createReserve(payload);
+		return result;
+	}
+
+	async listAll(payload) {
+		const result = await ReserveRepository.listReserve(payload);
+		return result;
+	}
+
+	async getById(payload) {
+		const result = await ReserveRepository.getReserve(payload);
+		return result; 
+	}
+
+	async update(paylaod, id ) {
+		const result = await ReserveRepository.updateReserve(paylaod, id);
+		return result;
+	}
+
+	async delete(payload) {
+		const result = await ReserveRepository.deleteReserve(payload); 
+	}
+}
+
+module.exports = new RentalService(); 
