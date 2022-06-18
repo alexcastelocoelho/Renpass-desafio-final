b952aa6a2a2b5e97c3875b23ecb26a647adda58a
Alex Castelo <alexcastelo437@gmail.com>
2022-06-18 20:15:10 -0300

    perf: improving error messages and adding cpf regex

--- src/app/validations/person/PersonValidate.js
+++ src/app/validations/person/PersonValidate.js
@@ -1,5 +1,5 @@
 const Joi = require('joi').extend(require('@joi/date'));
-const cpf = require('../../utils/cpfvalid');
+
 
 module.exports = async (req, res, next) => {
 	try{
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
 		const validPerson = Joi.object({
 			name:Joi.string().min(3).required(),
 			// eslint-disable-next-line no-useless-escape
-			cpf: Joi.string().max(14).required(),
+			cpf: Joi.string().regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/).required(),
 			birthday: Joi.date().format('DD/MM/YYYY').required(),
 			email: Joi.string().email().required(),
 			password: Joi.string().min(6).required(),
@@ -17,12 +17,17 @@ module.exports = async (req, res, next) => {
 
 		const {error} = await validPerson.validate(req.body, {abortEarly: false});
 		if (error) throw error;
-		if(!cpf(req.body.cpf)) {
-			throw new Error('cpf invalid');
-		}
+		// if(!cpf(req.body.cpf)) {
+		// 	throw new Error('Invalid CPF, check the format or enter a valid CPF');
+		// }
 		
 		return next();
 	} catch (error){		
-		return res.status(400).json({Error: error.message});
+		return res.status(400).json({
+			errors: error.details.map((alert) => ({
+				description: alert.message,
+				name: alert.path.join('.')
+			}))
+		});
 	}
 };
