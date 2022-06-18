3e92e63adc3c0517ecc08ba9c8be3f8b9c42b851
Alex Castelo <alexcastelo437@gmail.com>
2022-06-18 09:29:12 -0300

    fix: fixing car imports

--- src/routes/CarRouter.js
+++ src/routes/CarRouter.js
@@ -1,10 +1,11 @@
 const CarController = require('../app/controller/CarController');
-const validateCar = require('../app/validations/CarValidate');
+const ValidateCar = require('../app/validations/car/CarValidate');
+const ValidateUpdateCar = require('../app/validations/car/CarUpdateValidate');
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-	routes.post('/', validateCar ,CarController.creating);
+	routes.post('/', ValidateCar ,CarController.creating);
 	routes.get('/', CarController.listing);
 	routes.get('/:id', CarController.listone);
-	routes.put('/:id', CarController.updating);
+	routes.put('/:id', ValidateUpdateCar ,CarController.updating);
 	routes.patch('/:id/accessories/:idAccessorie', CarController.updatingAcessoryCar);
 	routes.delete('/:id', CarController.deleting);
 
