f5ec2cf89e931281457a8d5e72dafdd089c25481
Alex Castelo <alexcastelo437@gmail.com>
2022-06-20 13:20:10 -0300

    feat: configuring heroku port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 
 const ENV = 3000;
 
-app.listen(ENV, () => {
-	console.log(`server running on port ${ENV}`);
+console.log(`server running on port ${ENV}`);
+app.listen(process.env.PORT || ENV, () => {
 });
