c62f3f3224dfd4fde459c9b68693f6689056f68c
Alex Castelo <alexcastelo437@gmail.com>
2022-06-21 18:28:26 -0300

    feat: added put and post validation in reserves route

--- src/routes/ReserveRouter.js
+++ src/routes/ReserveRouter.js
@@ -1,10 +1,11 @@
 const ReserveController = require('../app/controller/ReserveController');
-
+const ReserveValidate = require('../app/validations/reserve/ReserveValidate');
+const ReserveUpdateValidate = require('../app/validations/reserve/ReserveUpdateValidate');
 module.exports = (server, routes, prefix = '/api/v1/rental/:id') => {
-	routes.post('/reserve',ReserveController.creating);
+	routes.post('/reserve',ReserveValidate ,ReserveController.creating);
 	routes.get('/reserve', ReserveController.listing);
 	routes.get('/reserve/:id', ReserveController.listone);
-	routes.put('/reserve/:id', ReserveController.updating);
+	routes.put('/reserve/:id', ReserveUpdateValidate ,ReserveController.updating);
 	routes.delete('/reserve/:id', ReserveController.deleting);
 
 
