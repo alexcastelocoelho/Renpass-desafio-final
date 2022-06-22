2e0483e553d538694a7baa93346a745d7fbc4d9a
Alex Castelo <alexcastelo437@gmail.com>
2022-06-22 00:49:31 -0300

    feat: created error for car id not found

---
+++ src/app/errors/CarErrors/IdNotFound.js
@@ -0,0 +1,11 @@
+class IdNotFound extends Error {
+	constructor(id) {
+		super();
+
+		this.statusCode = 400;
+		this.description = 'Id not Exist';
+		this.message = 'id not found';
+	} 
+}
+
+module.exports = IdNotFound;
