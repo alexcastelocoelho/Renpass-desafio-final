98a2f20834c449a90bb5fc45b9e32d6a5bd8bad0
Alex Castelo <alexcastelo437@gmail.com>
2022-06-23 10:28:24 -0300

    feat: created file to validate cnpj and consume viacep api

---
+++ src/app/utils/ViaCep.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+class ViaCep{
+	async  consumptionByZipCode(zipCode) {
+		const api = await axios.get(`https://viacep.com.br/ws/${zipCode}/json`);
+		return api.data;
+	}
+
+}
+module.exports = new ViaCep();
+//deploy
